76454d807cdca3c26dd274ab16040a6d59c91d9b
Cristiano Betta <cbetta@gmail.com>
2012-01-11 23:24:57 +0000

    Fix to resque.rb

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,6 +1,6 @@
 require 'resque'
 
-uri = "redis://127.0.0.1:6379"
+uri = URI.parse("redis://127.0.0.1:6379")
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
