e4dbf2ae7a0f2f9dc6f2090d0e0fd513af426d25
Cristiano Betta <cbetta@gmail.com>
2012-11-03 13:13:57 +0000

    Testign resque server

--- app/models/secure_resque_server.rb
+++ app/models/secure_resque_server.rb
@@ -3,7 +3,7 @@ require 'resque/server'
 class SecureResqueServer < Resque::Server
 
   use Rack::Auth::Basic, "Restricted Area" do |username, password|
-    [username, password] == [ENV['RESQUE_USERNAME'], ENV['RESQUE_PASSWORD']]
+    [username, password] == ["foo", "bar"]
   end
 
 end
