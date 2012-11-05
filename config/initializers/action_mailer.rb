a2eb9b7a02dcdd1e76ccfc1a0ce7c5da5b37964e
Cristiano Betta <cbetta@gmail.com>
2012-11-05 15:49:36 +0000

    Fixing mailer

--- config/initializers/action_mailer.rb
+++
@@ -1,8 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address        => "mail.cgb.im",
-  :port           => "2525",
-  :authentication => :plain,
-  :user_name      => ENV["SANTA_MAILER_USERNAME"],
-  :password       => ENV["SANTA_MAILER_PASSWORD"],
-  :enable_starttls_auto => false
-}
