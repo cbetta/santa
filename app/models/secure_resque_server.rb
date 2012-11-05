6d4b8485f46a10ed31ac7c0f017fa5e969d020ae
Cristiano Betta <cbetta@gmail.com>
2012-11-05 14:33:53 +0000

    Added resque secure setup

--- app/models/secure_resque_server.rb
+++ app/models/secure_resque_server.rb
@@ -3,7 +3,7 @@ require 'resque/server'
 class SecureResqueServer < Resque::Server
 
   use Rack::Auth::Basic, "Restricted Area" do |username, password|
-    [username, password] == ["foo", "bar"]
+    [username, password] == [ENV["RESQUE_USERNAME"], ENV["RESQUE_PASSWORD"]]
   end
 
 end
